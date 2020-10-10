c5b118605e6275a6a3beb690a2fa3cbc8e481518
Hanwen Cao <43282200+HanwenCao@users.noreply.github.com>
2020-10-10 17:58:34 +0800

    Update __init__.py

--- utils/data/__init__.py
+++ utils/data/__init__.py
@@ -5,5 +5,11 @@ def get_dataset(dataset_name):
     elif dataset_name == 'jacquard':
         from .jacquard_data import JacquardDataset
         return JacquardDataset
+    elif dataset_name == 'my':
+        from .my_data import MyDataset
+        return MyDataset
+    elif dataset_name == 'my2':
+        from .my_data2 import MyDataset2
+        return MyDataset2
     else:
         raise NotImplementedError('Dataset Type {} is Not implemented'.format(dataset_name))
